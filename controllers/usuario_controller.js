e06b3c3651672cf1ea0849432405d6bdeccc2779
CesarSantin <csrsntn@hotmail.com>
2019-08-24 06:46:36 -0500

    modificar usuario

--- controllers/usuario_controller.js
+++ controllers/usuario_controller.js
@@ -7,32 +7,50 @@ class UsuarioController {
     guardar(req, res) {
 
         var usuario = models.usuario;
+        if (req.body.external === "0") {
+            var datos = {
+                nombre: req.body.nombre,
+                apellido: req.body.apellido,
+                cedula: req.body.cedula,
+                correo: req.body.correo,
+                clave: req.body.clave,
+                externa_id: uuid.v4(),
+                rolId: req.body.rol
 
-        var datos = {
-            nombre: req.body.nombre,
-            apellido: req.body.apellido,
-            cedula: req.body.cedula,
-            correo: req.body.correo,
-            clave: req.body.clave,
-            externa_id: uuid.v4(),
-            rolId: req.body.rol
+            };
+            console.log(datos);
 
-        };
-        console.log(datos);
-        if (datos) {
             usuario.create(datos).then(function (newcuenta) {
-                var millisecondsToWait = 2500;
-                setTimeout(function () {
-                    res.redirect('http://localhost:3000/admin');
-                    console.log("******Registrado con exito******");
-                }, millisecondsToWait);
+                if (newcuenta) {
+                    var millisecondsToWait = 2500;
+                    setTimeout(function () {
+                        res.redirect('/perfil');
+                        console.log("******Registrado con exito******");
+                    }, millisecondsToWait);
+                }
+            }).catch(function (err) {
+                req.flash('error', 'No se pudo crear la personar');
+                res.redirect('/perfil');
             });
         } else {
-            var millisecondsToWait = 2500;
-            setTimeout(function () {
-                res.redirect('http://localhost:3000/admin');
-                console.log("Hubo un error");
-            }, millisecondsToWait);
+            usuario.update({
+                nombre: req.body.nombre,
+                apellido: req.body.apellido,
+                correo: req.body.correo,
+                clave: req.body.clave,
+                rolId: req.body.rol},
+                    {where: {externa_id: req.body.external}}).then(function (updateUsuario, created) {
+                if (updateUsuario) {
+                    var millisecondsToWait = 2500;
+                    setTimeout(function () {
+                        res.redirect('/perfil');
+                        console.log("******Modificado con exito******");
+                    }, millisecondsToWait);
+                }
+            }).catch(function (err) {
+                req.flash('error', 'No se pudo editar la personar');
+                res.redirect('/perfil');
+            });
         }
         ;
     }
