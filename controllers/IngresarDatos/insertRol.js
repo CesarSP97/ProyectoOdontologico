1818ebc1b8b31c4021bad0d0b9ad76434dd2f7d2
CesarSantin <csrsntn@hotmail.com>
2019-08-24 19:42:01 -0500

    css

--- controllers/IngresarDatos/insertRol.js
+++ controllers/IngresarDatos/insertRol.js
@@ -1,22 +1,30 @@
 var models = require('../../models/');
+var uuid = require('uuid');
 var Rol = models.rol;
+var Usuario = models.usuario;
+
 var insertRol = function () {
     Rol.findOrCreate({where: {nombre: 'ODONTOLOGO'},
         defaults: {
-            id: '1',
-            job: 'ODONTOLOGO'}}).spread((user, created) => {
-    });
+            id: '1'}});
 
     Rol.findOrCreate({where: {nombre: 'SECRETARIA'},
         defaults: {
-            id: '2',
-            job: 'SECRETARIA'}}).spread((user, created) => {
-    });
+            id: '2'}});
 
     Rol.findOrCreate({where: {nombre: 'ADMINISTRADOR'},
         defaults: {
-            id: '3',
-            job: 'ADMINISTRADOR'}}).spread((user, created) => {
-    });
+            id: '3'}});
+
+    Usuario.findOrCreate({where: {rolId: '3'},
+        defaults: {
+            id: '1',
+            nombre: 'Admin',
+            apellido: 'Admin',
+            cedula: '9999999999',
+            correo: 'admin@hotmail.com',
+            clave: '1234',
+            externa_id: uuid.v4()
+        }});
 };
 module.exports = insertRol();
